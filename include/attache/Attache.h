102545036892a3ab71d32c06b17a0da98af5dca2
Jan Winkler <winkler@cs.uni-bremen.de>
2015-08-11 13:55:32 +0200

    Attache update

--- include/attache/Attache.h
+++ include/attache/Attache.h
@@ -4,6 +4,9 @@
 
 // System
 #include <stdio.h>
+#include <iostream>
+#include <map>
+#include <string>
 
 // Boost
 #include <boost/bind.hpp>
@@ -13,10 +16,23 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 
+// ROS
+#include <ros/ros.h>
+#include <tf/tfMessage.h>
+
+// Attachment Messages
+#include <attache_msgs/Attachment.h>
+
 
 namespace gazebo {
   class Attache : public WorldPlugin {
   private:
+    std::map< std::string, std::map<std::string, physics::JointPtr> > m_mapJoints; 
+    
+    ros::NodeHandle m_nhHandle;
+    ros::ServiceServer m_srvAttach;
+    ros::ServiceServer m_srvDetach;
+    
     physics::WorldPtr m_wpWorld;
     event::ConnectionPtr m_cpUpdateConnection;
     
@@ -27,6 +43,11 @@ namespace gazebo {
     
     void Load(physics::WorldPtr wpWorld, sdf::ElementPtr epPtr);
     void OnUpdate(const common::UpdateInfo &uiInfo);
+    
+    bool deleteJointIfPresent(std::string strLink1, std::string strLink2);
+    
+    bool serviceAttach(attache_msgs::Attachment::Request &req, attache_msgs::Attachment::Response &res);
+    bool serviceDetach(attache_msgs::Attachment::Request &req, attache_msgs::Attachment::Response &res);
   };
   
   GZ_REGISTER_WORLD_PLUGIN(Attache)
--- src/Attache.cpp
+++ src/Attache.cpp
@@ -2,15 +2,110 @@
 
 
 namespace gazebo {
+  Attache::Attache() : m_nhHandle("~") {
+  }
+  
+  Attache::~Attache() {
+  }
+  
   void Attache::Load(physics::WorldPtr wpWorld, sdf::ElementPtr epPtr) {
     this->m_wpWorld = wpWorld;
     
-    // TODO: Set up ROS services
+    m_srvAttach = m_nhHandle.advertiseService<Attache>("attach", &Attache::serviceAttach, this);
+    m_srvDetach = m_nhHandle.advertiseService<Attache>("detach", &Attache::serviceDetach, this);
     
     this->m_cpUpdateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&Attache::OnUpdate, this, _1));
+    
+    std::cout << "Attache plugin loaded" << std::endl;
   }
   
   void Attache::OnUpdate(const common::UpdateInfo &uiInfo) {
-    // TODO: spinOnce
+    ros::spinOnce();
+  }
+  
+  bool Attache::deleteJointIfPresent(std::string strLink1, std::string strLink2) {
+    bool bReturnvalue = false;
+    
+    if(m_mapJoints.find(strLink1) != m_mapJoints.end()) {
+      if(m_mapJoints[strLink1].find(strLink2) != m_mapJoints[strLink1].end()) {
+	physics::JointPtr jpJoint = m_mapJoints[strLink1][strLink2];
+	
+	jpJoint->Reset();
+	jpJoint->Detach();
+	jpJoint->Fini();
+	
+	m_mapJoints[strLink1].erase(strLink2);
+	
+	if(m_mapJoints[strLink1].size() == 0) {
+	  m_mapJoints.erase(strLink1);
+	}
+	
+	bReturnvalue = true;
+      }
+    }
+    
+    return bReturnvalue;
+  }
+  
+  bool Attache::serviceAttach(attache_msgs::Attachment::Request &req, attache_msgs::Attachment::Response &res) {
+    std::string strLink1 = req.model1 + "." + req.link1;
+    std::string strLink2 = req.model2 + "." + req.link2;
+    
+    gazebo::physics::ModelPtr mpLink1 = this->m_wpWorld->GetModel(req.model1);
+    
+    if(mpLink1) {
+      const physics::LinkPtr lpLink1 = mpLink1->GetLink(req.link1);
+      
+      if(lpLink1) {
+	gazebo::physics::ModelPtr mpLink2 = this->m_wpWorld->GetModel(req.model2);
+	
+	if(mpLink2) {
+	  const physics::LinkPtr lpLink2 = mpLink2->GetLink(req.link2);
+	  
+	  if(lpLink2) {
+	    this->deleteJointIfPresent(strLink1, strLink2);
+	    this->deleteJointIfPresent(strLink2, strLink1);
+	    
+	    std::cout << "[Attache] Attach link '" << lpLink1->GetName() << "' to link '" << lpLink2->GetName() << "'" << std::endl;
+	    
+	    m_mapJoints[strLink1][strLink2] = this->m_wpWorld->GetPhysicsEngine()->CreateJoint("revolute", mpLink1);
+	    m_mapJoints[strLink1][strLink2]->Load(lpLink1, lpLink2, math::Pose());
+	    
+	    m_mapJoints[strLink1][strLink2]->Init();
+	    m_mapJoints[strLink1][strLink2]->SetHighStop(0, 0);
+	    m_mapJoints[strLink1][strLink2]->SetLowStop(0, 0);
+	    
+	    res.success = true;
+	  } else {
+	    std::cerr << "[Attache] No link '" << req.model2 << "." << req.link2 << "'" << std::endl;
+	  }
+	} else {
+	  std::cerr << "[Attache] No model '" << req.model2 << "'" << std::endl;
+	}
+      } else {
+	std::cerr << "[Attache] No link '" << req.model1 << "." << req.link1 << "'" << std::endl;
+      }
+    } else {
+      std::cerr << "[Attache] No model '" << req.model1 << "'" << std::endl;
+    }
+    
+    return true;
+  }
+  
+  bool Attache::serviceDetach(attache_msgs::Attachment::Request &req, attache_msgs::Attachment::Response &res) {
+    std::string strLink1 = req.model1 + "." + req.link1;
+    std::string strLink2 = req.model2 + "." + req.link2;
+    
+    if(this->deleteJointIfPresent(strLink1, strLink2) || this->deleteJointIfPresent(strLink2, strLink1)) {
+      std::cout << "[DUMMY] Detached link '" << strLink1 << "' from link '" << strLink2 << "'" << std::endl;
+      
+      res.success = true;
+    } else {
+      std::cerr << "[Attache] No connection to detach between '" << strLink1 << "' and '" << strLink2 << "'" << std::endl;
+      
+      res.success = false;
+    }
+    
+    return true;
   }
 }
